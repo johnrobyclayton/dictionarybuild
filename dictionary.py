300bbfb0190110d66d0990139ec6675287079d5b
John Clayton <claytonjohnroby@techie.com>
2024-10-01 22:22:00 +1000

    7

--- dictionary.py
+++ dictionary.py
@@ -2,41 +2,74 @@ class POS:
     """
     Load,create,edit,save Parts of Speech
     A Part of Speech is the gramatical purpose of a word or phrase in a sentence
+    members:
+        posstr: a string identifying the part of speech
+        descriptionstr: a string describing the part of speech
+        descriptionlst: a list of Word objects and Phrase objects that make up the description
+        wordlst: a list of Word objects that are this part of speech observed in corpora
+        phraselst: a likst of Phrase objects that are this part of speech observed in corpora
     """
-    def __init__(self,posstr,descriptionstr,wordlst=[],descriptionlst=[]):
+    def __init__(self,posstr,descriptionstr,wordlst=[],phraselst=[],descriptionlst=[]):
         self.posstr=posstr
         self.descriptionstr=descriptionstr
         self.wordlst=list()
-        for wordstr in wordlst:
-            self.wordlst.append(wordstr)
-        for descriptiontkn in descriptionlst:
-            self.descriptionlst.append(descriptiontkn)
+        for wordobj in wordlst:
+            self.wordlst.append(wordobj)
+        for phraseobj in phraselst:
+            self.phraselst.append(phraseobj)
+        for descriptionobj in descriptionlst:
+            self.descriptionlst.append(descriptionobj)
 
-class Wordgroup:
+class WordPhraseGroup:
     """
     Load,create,edit,save wordgroups
     A Wordgroup is a group of words that are interchangeable in some way within a text
     They might be different things that can replace each other and result in a
     gramatical and/or a sensible sentence
+    Members:
+        posobj: POS object identifying a part of speech
+        descriptionstr: description of the words and phrases that make up this group
+        contextlst: the list of context objects where these word objects and phrase 
+            objects can be found
+        wordlst: the list of Word objects in this WordPhraseGroup
+        phraselst: the list of Phrase objects in this WordPhraseGroup
+        descriptionlst: The list of Word objects and Phrase objects that make up the 
+            descriptionstr 
     """
-    def __init__(self,posstr,descriptionstr,wordlst=[],descriptionlst=[]):
-        self.posstr=posstr
+    def __init__(self,posobj,descriptionstr,contextlst=[],wordlst=[],phraselst=[],descriptionlst=[]):
+        self.posobj=posobj
         self.descriptionstr=descriptionstr
         self.wordlst=list()
-        for wordstr in wordlst:
-            self.wordlst.append(wordstr)
-        for descriptiontkn in descriptionlst:
-            self.descriptionlst.append(descriptiontkn)
-            
+        for wordobj in wordlst:
+            self.wordlst.append(wordobj)
+        for phraseobj in phraselst:
+            self.wordlst.append(phraseobj)
+        for descriptionobj in descriptionlst:
+            self.descriptionlst.append(descriptionobj)
+
+class SpeechPart:
+    """
+    Load,create,edit,save SpeechPart objects
+    A SpeechPart is a superclass of Word and Phrase classes
+    Senteces are made up of Words and Phrases that each have a specific POS
+    Each SpeechPart has a particular meaning and the combinations of meanings 
+    related to each other by their Part Of Speech creates the meaning of the 
+    sentence
+    """    
+    def __init__(self, posstr=None,posobj=None,definitionstr=None,definitionlst=[],usagelst=[]):
+        
+        self.posstr=posstr
+        self.posobj=posobj
+        self.definitionstr=definitionstr
+        self.definitionlst=list()
+         
+                
 class Phrase:
     """
     Load,create,edit,save phrases
     A Part Of speech can be occupied by a series of words or other phrases. 
     """
     def __init__(self,phrasestr,posstr,definitionstr,phraselst=[],definitionlst=[],usagelst=[]):
-        self.phrasestr=phrasestr
-        self.posstr=posstr
-        self.definitionstr=definitionstr
         self.phraselst=list()
         for phrasetkn in phraselst:
             self.phraselst.append(phrasetkn)
