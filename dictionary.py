43760efd59ad99b337b6d7da02aa6a9ae7227811
John Clayton <claytonjohnroby@techie.com>
2024-10-04 22:51:42 +1000

    11

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,19 @@
 import copy
+import nltk
+from nltk.text import Text
+from nltk.tokenize import word_tokenize
 class POS:
     """
     Load,create,edit,save Parts of Speech
     A Part of Speech is the gramatical purpose of a word or phrase in a sentence
     members:
-        posstr: a string identifying the part of speech
-        posobj: a instance of a POS class
         descriptionstr: a string describing the part of speech
-        descriptionlst: an ordered list of SpeechParts objects, which can be either Word objects of Phrase objects 
-            that make up the description
-        speechpartlst: a list of speechpart objects that are this part of speech observed in corpora
+        descriptionlst: an ordered list of SpeechParts objects, 
+            which can be either Word objects or 
+            Phrase objects that make up the description
+        speechpartlst: a list of speechpart objects that are this 
+            part of speech observed in corpora
+        posstr: a string identifying the part of speech
         
     """
     def isValid(self):
@@ -18,24 +22,7 @@ class POS:
             descriptionUnpacked+=str(speechpartobj)
         return self.descriptionstr==descriptionlstUnpacked
 
-    def __init__(self,*,posstr=None,descriptionstr=None,speechpartset=[],descriptionlst=[]):
-        def signature():
-            """
-            generates a constructor signature that is used to select a function for creating the object.
-                Parameters:
-                    The parameters of the __init__ function
-                Returns:
-                    For each parameter in the __init__ function
-                        assign a unique power of 2 value
-                        if the type of the parameter in None or empty set,dict,list set the value to 0 
-                        else set the value to the power of 2
-                        Sum all the values
-                        return the sum for the signature
-            """
-            
-            
-            
-        
+    def __init__(self,*,descriptionstr=None,descriptionlst=[],speechpartset=[],posstr=None):
         self.posstr=posstr
         self.descriptionstr=descriptionstr
         self.wordlst=list()
@@ -51,13 +38,12 @@ class SpeechPartGroup:
     They might be different things that can replace each other and result in a
     gramatical and/or a sensible sentence
     Members:
-        posobj: POS object identifying a part of speech
         descriptionstr: description of the words and phrases that make up this group
+        descriptionlst: The list of SpeechPart objects that make up the 
+            descriptionstr 
         contextset: the set of context objects where these word objects and phrase 
             objects can be found
         speechpartset: the set of SpeechPart objects in this SpeechPartGroup
-        descriptionlst: The list of SpeechPart objects that make up the 
-            descriptionstr 
     """
     def isValid(self):
         descriptionlstUnpacked=str()
@@ -65,8 +51,8 @@ class SpeechPartGroup:
             descriptionUnpacked+=str(speechpartobj)
         return self.descriptionstr==descriptionlstUnpacked
 
-    def __init__(self,descriptionstr= None,contextset=set(),speechpartset=set(),descriptionlst=[]):
-        self.posobj=posobj
+    def __init__(self,*,descriptionstr= None,descriptionlst=[],\
+            contextset=set(),speechpartset=set()):
         self.descriptionstr=descriptionstr
         self.contextset=set()
         for contextobj in contextset:
@@ -87,23 +73,22 @@ class SpeechPart:
     related to each other by their Part Of Speech creates the meaning of the 
     sentence
     Members:
-        posstr the string identifying its part of speech
-        posobj The POS object describing its part of speech
         definitionstr The string of its definition
         definitionlst The list of SpeechPart objects making up the definition.
+        posstr the string identifying its part of speech
         usageset the set of usage objects that describe where this SpeechPart shows in corpora
     """    
     def isValid(self):
-        descriptionlstUnpacked=str()
+        definitionlstUnpacked=str()
         for speechpartobj in self.definitionlst:
             definitionlstUnpacked+=str(speechpartobj)
-        return self.desfinitionstr==definitionlstUnpacked
+        return self.definitionstr==definitionlstUnpacked
 
-    def __init__(self, posstr=None,definitionstr=None,definitionlst=[],usageset=set()):
+    def __init__(self,*,definitionstr=None,definitionlst=[], \
+            posstr=None,usageset=set()):
         
         
         self.posstr=posstr
-        self.posobj=posobj
         self.definitionstr=definitionstr
         self.definitionlst=list()
         for speechpartobj in definitionlst:
@@ -120,21 +105,30 @@ class Phrase(SpeechPart):
     A Part Of speech can be occupied by a series of words or other phrases. 
     Members:
         phrasestr the string representation of the phrase
-        phraseobj a phrase object
-        posstr the string identifying the part of speech
-        posobj the POS object describing the part of speech
+        phraselst the list of speechpart objects that make up the phrase
         definitionstr the string with the definition of the phrase
-        speechpartlst the list of speechpart objects that make up the phrase
         definitionlst the list of speechpart objects that make up the definition.
+        posstr the string identifying the part of speech
         usagelist the list of usage objects that describe where this phrase shows up in corpora
     """
-    def __init__(self,phrasestr=None,phraseobj=None,posstr=None,posobj=None,definitionstr=None,speechpartlst=[],definitionlst=[],usageset=set()):
+    def isValid(self):
+        definitionlstUnpacked=str()
+        for speechpartobj in self.definitionlst:
+            definitionlstUnpacked+=str(speechpartobj)
+        phraselstUnpacked=str()
+        for speechpartobj in self.phraselst:
+            phraselstUnpacked+=str(speechpartobj)
+        return self.definitionstr==definitionlstUnpacked and \
+            self.phrasestr==phraselstUnpacked
+
+    def __init__(self,*,phrasestr=None,phraselst=[],\
+            definitionstr=None,definitionlst=[],\
+            usageset=set(),posstr=None):
         self.phrasestr=phrasestr
-        self.phaseobj=phraseobj
-        SpeechPart.__init__(posstr,posobj,definitionstr,definitionlst,usageset)
         self.phraselst=list()
-        for speechpartobj in speechpartlst:
+        for speechpartobj in phraselst:
             self.phraselst.append(speechpartobj)
+        SpeechPart.__init__(self,posstr=posstr,definitionstr=definitionstr,definitionlst=definitionlst,usageset=usageset)
         
 class Word(SpeechPart):
     """
@@ -142,20 +136,19 @@ class Word(SpeechPart):
     A Part Of speech can be occupied by a word. 
     Members:
         wordstr the string representation of the phrase
-        wordobj a phrase object
-        posstr the string identifying the part of speech
-        posobj the POS object describing the part of speech
         definitionstr the string with the definition of the phrase
-        speechpartlst the list of speechpart objects that make up the phrase
         defiitionlst the list of speechpart objects that make up the definition.
-        usagelist the list of usage objects that describe where this phrase shows up in corpora
+        posstr the string identifying the part of speech
+        usageset the set of usage objects that describe where this phrase shows up in corpora
     """
-    def __init__(self,wordstr=None,wordobj=None,posstr=None,posobj=None,definitionstr=None,usagestr=None,usageset=set(),definitionlst=[]):
-        self.wortstr=wordstr
-        self.wordobj=wordobj
-        SpeechPart.__init__(posstr,posobj,definitionstr,definitionlst,usageset)
+    def __init__(self,*,wordstr=None,\
+            definitionstr=None,definitionlst=[],\
+            posstr=None,usageset=set()):
+        self.wordstr=wordstr
+        SpeechPart.__init__(self,posstr=posstr,definitionstr=definitionstr,definitionlst=definitionlst,usageset=usageset)
         
-    
+    def __str__(self):
+        return f'{self.wordstr}'    
             
 class Usage:
     """
@@ -164,14 +157,24 @@ class Usage:
     A Usage is made up of words, phrases and wordgroups that have particular parts of speech surrounding
     a word or phrase that identify the contexts where the word or phrase occurs and can be used to identify its pos 
     Members:
-        corpora an object describing the source of the usage of the speechpart for which this is a usage of
+        corporaobj an object describing the source of the usage of the speechpart for which this is a usage of
         prespeechpartlst the list of speechparts before the speechpart for which this is a usage of
         speechpart the speechpart for which this is a usage of
         postspeechpartlst the list of speechparts after the speechpart for which this is a usage of
         sourcestr the string from the corpora that is the source of this usage
     """
-    def __init__(self,corpora=None,prespeechpartlst=[],thisspeechpartobj=None,postspeechpartlst=[],sourcestr=None):
-        self.corpora=corpora
+    def isValid(self):
+        sourcestrUnpacked=str()
+        for speechpartobj in self.prespeechpartlst:
+            sourcestrUnpacked+=str(speechpartobj)
+        sourcestrUnpacked+=str(self.thisspeechpartobj)
+        for speechpartobj in self.postspeechpartlst:
+            sourcestrUnpacked+=str(speechpartobj)
+        return sourcestrUnpacked==self.sourcestr
+
+    def __init__(self,*,corporaobj=None,prespeechpartlst=[],\
+            thisspeechpartobj=None,postspeechpartlst=[],sourcestr=None):
+        self.corpora=corporaobj
         self.prespechpartlst=list()
         for speechpartobj in prespeechpartlst:
             self.prespeechpartlst.append(speechpartobj)
@@ -189,25 +192,37 @@ class Corpora:
     A Corpora can be represented as a string of text, a list of strings of text.
     A corpora can be stored in a file
     Members:
-    namestr a string that identifies the corpora
-    metadatastr a string that contains the mettadata for the corpora
     corporastr a string representation of the corpora
-    speechpartlst a list of speechparts representing the corpora
+    corporalst a list of speechparts representing the corpora
     descriptionstr a string describing the corpora
     descriptionlst a list of speechparts that describe the corpora
+    namestr a string that identifies the corpora
+    metadatastr a string that contains the mettadata for the corpora
     filespec the specitifation of the file containing the corpora
     """        
-    def __init__(self,namestr=None,metadatastr=None,corporastr=None,speechpartlst=[],descriptionstr=None,descriptionlst=[],filespec=None):
-        self.namestr=namestr
-        self.metadatastr=metadatastr
+    def isValid(self):
+        descriptionlstUnpacked=str()
+        for speechpartobj in self.descriptionlst:
+            descriptionlstUnpacked+=str(speechpartobj)
+        corporalstUnpacked=str()
+        for speechpartobj in self.corporalst:
+            phraselstUnpacked+=str(speechpartobj)
+        return self.descriptionstr==descriptionlstUnpacked and \
+            self.corporastr==corporalstUnpacked
+
+    def __init__(self,*,corporastr=None,corporalst=[],\
+            descriptionstr=None,descriptionlst=[],\
+            namestr=None,metadatastr=None,filespec=None):
         self.corporastr=corporastr
-        self.speechpartlst=list()
-        for speechpartobj in speechpartlst:
-            self.speechpartlst.append(speechpartobj)
+        self.corporalst=list()
+        for speechpartobj in corporalst:
+            self.corporalst.append(speechpartobj)
         self.descriptionstr=descriptionstr
         self.descriptionlst=list()
         for speechpartobj in descriptionlst:
             self.descriptionlst.append(speechpartobj)
+        self.namestr=namestr
+        self.metadatastr=metadatastr
         self.filespec=filespec
             
 class Definition:
@@ -224,13 +239,20 @@ class Definition:
         definitionlst a list of speechparts that make up the definition
         speechpartset the set of speechparts that have this definition
     """
-    def __init__(self,definitionstr=None,posstr=None,posobj=None,definitionlst=[],speechpartset=set()):
-        self.definitionstr-definitionstr
+    def isValid(self):
+        definitionlstUnpacked=str()
+        for speechpartobj in self.definitionlst:
+            definitionlstUnpacked+=str(speechpartobj)
+        return self.definitionstr==definitionlstUnpacked
+
+    def __init__(self,*,definitionstr=None,definitionlst=[],\
+            posstr=None,speechpartset=set()):
+        self.definitionstr=definitionstr
+        if definitionlst==[]:
+            self.definitionlst=list()
+            for token in nltk.tokenize.word_tokenize(self.definitionstr):
+                self.definitionlst.append(Word(wordstr=token,definitionstr=None,definitionlst=[],posstr=None,usageset=set()))    
         self.posstr=posstr
-        self.posobj=posobj
-        self.definitionlst=list()
-        for speechpartobj in definitionlst:
-            self.definitionlst.append(speechpartobj)
         self.speechpartset=set()
         for speechpartobj in speechpartset:
             self.speechpartset.add(speechpartobj)
@@ -255,3 +277,19 @@ class Dictionary:
         for speechpartobj in speechpartset:
             speechpartset.addspeechpartobj
     
+def main():
+    w=Word(wordstr='word',definitionstr='A word',definitionlst=[],posstr='noun',usageset=set())
+    print(w.wordstr,w.definitionstr,w.posstr)
+    p=Phrase(phrasestr='long word',definitionstr='A long word',definitionlst=[],posstr='noun',usageset=set())
+    print(p.phrasestr,p.definitionstr,p.posstr)
+    d=Definition(definitionstr='A string of text that has no spaces and has a meaning',definitionlst=[],posstr='noun',speechpartset=set())
+    print(d.definitionstr)
+    print(1)
+    w=Word(wordstr=w.wordstr,definitionstr=d.definitionstr,definitionlst=[],posstr='noun',usageset=set())
+    print(2)
+    print(w.wordstr,w.definitionstr,w.posstr)
+    for word in d.definitionlst:
+        print(word)
+    print(3)
+if __name__ == '__main__':
+    main()
