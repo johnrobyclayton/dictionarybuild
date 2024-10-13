afc358cfbe29919381b8b3c49eecfbfc28a22ed2
johnrobyclayton <30948898+johnrobyclayton@users.noreply.github.com>
2024-10-13 22:38:43 +1100

    21

--- dictionary.py
+++ dictionary.py
@@ -3,8 +3,8 @@ import nltk
 from nltk.text import Text
 from nltk.tokenize import word_tokenize
 import csv
-
-
+import pickle
+import os
 
 class Compare:
     """
@@ -328,7 +328,7 @@ class Phrase(SpeechPart,Compare):
         
     """
 
-    def __repr__(self,depth):
+    def __repr__(self,*,depth=0):
         if depth>0:
             depth-=1
             return f"Phrase:{{"+\
@@ -377,7 +377,7 @@ class Word(SpeechPart,Compare):
         posstr the string identifying the part of speech
         usageset the set of usage objects that describe where this phrase shows up in corpora
     """
-    def __repr__(self,depth):
+    def __repr__(self,depth=0):
         if depth>0:
             depth-=1
             return f"Word:{{"+\
@@ -597,6 +597,10 @@ class Definition(Compare):
         self.speechpartset = speechpartset if isinstance(speechpartset, set) and speechpartset != set() else set()
         self.hasspeechpartset = isinstance(self.speechpartset, set) and self.speechpartset != set() and all(isinstance(obj, SpeechPart) for obj in self.speechpartset)
 
+        self.defindint=int()
+        
+    def definitionContains(self,str):
+        return str in self.definitionstr 
 
                 
                 
@@ -677,15 +681,73 @@ class Dictionary:
         self.posphraseset = posphraseset if isinstance(posphraseset, set) and posphraseset != set() else set()
         self.hasposphraseset = isinstance(self.posphraseset, set) and self.posphraseset != set() and all(isinstance(obj, POSPhrase) for obj in self.usageset)
 
-    def loadDefinitions(self):
-        with open('corpus/dictionary.csv',newline='\n') as csvfile:
+    def loadDefinitions_csv(self):
+        with open('dictionary/OPTED-dictionary.csv',newline='\n') as csvfile:
             definitionreader = csv.reader(csvfile, dialect="excel",delimiter=',',quotechar='"')
-            for word,pos,definition in definitionreader:
-                speechpartset=set()
-                speechpartset.add(Word(wordstr=word))
-                self.definitionset.add(Definition(definitionstr=definition,partofgrammarstr=pos,speechpartset=speechpartset))
-
+            #for word,count,pos,definition in definitionreader:
+            for line in definitionreader:
+                if len(line)==4:
+                    #create a speechpartset to hold the words or phrases that head the dictionary entries
+                    speechpartset=set()
+                    #for each entry in the dictionary add the word, part of speech and definition to the speechpartset full of Word objects 
+                    speechpartset.add(Word(wordstr=line[0],partofgrammarstr=line[2],definitionstr=line[3]))
+                    #for each entry in the dictionary add a definition along with the part of speech and the word or phrase connected to the entry
+                    self.definitionset.add(Definition(definitionstr=line[3],partofgrammarstr=line[2],speechpartset=speechpartset))
+                    #add the speechpartset generated from an entry to the accumulating speechpartset in the dictionary
+                    self.speechpartset=self.speechpartset.union(speechpartset)
+                    
+    def saveDefinitions_pickle(self):
+        filehandler=open("dictionary/dictionaryobj.pkl","wb")
+        pickle.dump(self,filehandler)
     
+    def loadDefinition_pickle():
+        filehandler=open("dictionary/dictionaryobj.pkl","rb")
+        return pickle.load(filehandler)
+        
+    def selectADefinition(self,*,definitionlst=[]):
+        for definition in definitionlst:
+            if isinstance(definition,Definition):
+                print(f''+\
+                      f'Definition ident: {definition.defindint} \n'+\
+                      f'Definition POS: {definition.partofgrammarstr} \n'+\
+                      f'Definition string: {definition.definitionstr} \n'
+                    )
+        print('Enter an Definitioin ident to edit:')
+        ident=input()
+        editDefinition=Definition()
+        for definition in definitionlst:
+            if isinstance(definition, Definition):
+                if definition.defindint==ident:
+                    editDefinition=definition
+        return editDefinition
+        
+    def investigate(self):
+        cont=True
+        while cont:
+            print('Enter selection')
+            print('Enter a word to find a dictionary entry for that word 1')
+            print('Enter a word to find dictionary entries with the word in the definition 2')
+            print('Enter a number to find a dictionary entry with that index 3')
+            print('quit q')
+            command = input()
+            definitioneditlist=list()
+            if command == '1':
+                print('Enter a word')
+                inputstr= input()
+                for definition in self.definitionset:
+                    if isinstance(definition,Definition):
+                        if inputstr in definition.definitionstr:
+                            definitioneditlist.append(definition)
+                selecteddefinitioin=self.selectADefinition(definitionlst=definitioneditlist)
+                print(selecteddefinitioin.definitionstr)
+            elif command == '2':
+                inputstr= input()
+            elif command == '3':
+                inputint= int(input())
+            elif command == 'q':
+                cont=False
+            else:
+                print('invalid selection, q to quit')
 
 
 
@@ -697,15 +759,33 @@ def main():
     #c=Compare(sstr='a aa aaa b bb bbb',llst=['a','aa','aaa','b','bb','bbb'])
     #print
     d=Dictionary(dictionaryfle=None,speechpartset=None)
-    w=Word(wordstr='word',definitionstr='A word',definitionlst=[],partofgrammarstr='Noun',usageset=set(),dictionary=d)
+    #w=Word(wordstr='word',definitionstr='A word',definitionlst=[],partofgrammarstr='Noun',usageset=set(),dictionary=d)
     #print('d',d.entries())
-    print('Word members\n',w.wordstr,w.definitionstr,w.partofgrammarstr)
-    print('word string:',str(w))
-    print(f'word repr:\n{w.__repr__(1)}')
-    print(w.definitionMatches)
-    print(d.__repr__())
-    d.loadDefinitions()
-    print(len(d.definitionset))
+    #print('Word members\n',w.wordstr,w.definitionstr,w.partofgrammarstr)
+    #print('word string:',str(w))
+    #print(f'word repr:\n{w.__repr__(1)}')
+    #print(w.definitionMatches)
+    #print(d.__repr__())
+    d.loadDefinitions_csv()
+    #print(len(d.definitionset))
+    #d.saveDefinitions_pickle()
+    #1del(d)
+    #d=Dictionary()
+    #d=Dictionary.loadDefinition_pickle()
+    #d.investigate()
+    index=0
+    if isinstance(d,Dictionary):
+        print(len(d.definitionset))
+        for definition in d.definitionset:
+            if isinstance(definition,Definition):
+                index+=1
+                definition.defindint=index
+    d.saveDefinitions_pickle()
+    #d=Dictionary.loadDefinition_pickle()
+    #d.investigate()
+    #d.saveDefinitions_pickle()
+                    
+    #print(len(d.speechpartset))
     #p=Phrase(phrasestr='long word',definitionstr='A long word',definitionlst=[],posstr='noun',usageset=set())
     #print(p.phrasestr,p.definitionstr,p.posstr)
     #d=Definition(definitionstr='A string of text that has no spaces and has a meaning',definitionlst=[],posstr='noun',speechpartset=set())
